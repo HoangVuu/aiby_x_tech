30a1aa0f7fb39ae73c804cf412d0ac0b171d23be
Hoang Vu <vu@cloudkinetix.com>
2025-04-02 00:05:22 +0700

    fix: Responsive of landing page

--- src/app/our-dna/page.tsx
+++ src/app/our-dna/page.tsx
@@ -30,7 +30,7 @@ export default function OurDNA() {
             style={{
               opacity: "1",
               width: "100%",
-              // height: "100%",
+              height: "100%",
               objectFit: "cover",
             }}
             alt={"hero"}
