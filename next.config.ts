3bb3647ef4bf0e90c2eb768dba2f7d17edd69204
Nguyen Huy <huyn200028@gmail.com>
2025-04-02 17:57:37 +0700

    fix icon website

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // uncomment when deploy to matbao
-  // output: "export",
-  // trailingSlash: true,
-  // images: {
-  //   loader: "akamai",
-  //   path: "/",
-  // },
+  output: "export",
+  trailingSlash: true,
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
 };
 
 export default nextConfig;
